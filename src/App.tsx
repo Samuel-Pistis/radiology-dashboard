cd91ef4489ed4ec9ea812cc488ce5704e2b63288
Samuel-Pistis <faithsamuelolusegun@gmail.com>
2026-02-25 14:10:30 +0100

    Fix unused imports for Vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider, useAppContext } from './context/AppContext';
 import { Layout } from './components/Layout';
 import { Dashboard } from './pages/Dashboard';
